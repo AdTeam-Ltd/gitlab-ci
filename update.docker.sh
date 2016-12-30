5ef62a1228a35cdc93c90791deb6e56e56c9fd03
root <root@gitlab.teamhst.net>
2016-12-30 14:19:48 +0300

    Change Dockerfile

--- update.docker.sh
+++ update.docker.sh
@@ -2,8 +2,8 @@
 
 # Меняем тут файл и бахаем
 
-#docker build -t adteam/gitlab-ci .
-#docker push adteam/gitlab-ci
+docker build -t adteam/gitlab-ci .
+docker push adteam/gitlab-ci
 git add .
 git commit -m "Change Dockerfile"
 git push origin master
